61bb2d9fe2b9a2f408958af78b6d5b24affd6d17
c4nnh <cannh.99@gmail.com>
2023-03-17 00:06:38 +0700

    add tailwind

---
+++ app/util/db.server.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from "@prisma/client";
+import invariant from "tiny-invariant";
+
+let prisma: PrismaClient;
+
+declare global {
+  var __db__: PrismaClient;
+}
+
+if (process.env.NODE_ENV === "production") {
+  prisma = getClient();
+} else {
+  if (!global.__db__) {
+    global.__db__ = getClient();
+  }
+  prisma = global.__db__;
+}
+
+function getClient() {
+  const { DATABASE_URL } = process.env;
+  invariant(typeof DATABASE_URL === "string", "DATABASE_URL env var not set");
+
+  const databaseUrl = new URL(DATABASE_URL);
+
+  const client = new PrismaClient({
+    datasources: {
+      db: {
+        url: databaseUrl.toString(),
+      },
+    },
+  });
+  // connect eagerly
+  client.$connect();
+
+  return client;
+}
+
+export { prisma };
