32f9fabe459be5731cfe2e4a8831d26d0c9bbe0d
c4nnh <cannh.99@gmail.com>
2023-04-04 13:22:16 +0700

    fix cron

--- app/routes/cron-jobs+/vercel.ts
+++ app/routes/cron-jobs+/vercel.ts
@@ -2,5 +2,4 @@ import type { LoaderFunction } from '@remix-run/node'
 
 export const loader: LoaderFunction = async () => {
   console.log('Cron job on vercel')
-  return new Response('Hello Cron!')
 }
