b408156524394f6a082bd97e3a0c3c3267873151
c4nnh <cannh.99@gmail.com>
2023-04-10 10:02:36 +0700

    add endpoints upstash

--- app/routes/cron-jobs+/test-deploy-quirrel.ts
+++ app/routes/cron-jobs+/test-deploy-quirrel.ts
@@ -1,9 +1,3 @@
-import { CronJob } from 'quirrel/remix'
-
-export const action = CronJob(
-  'cron-jobs/test-deploy-quirrel',
-  ['* * * * *', 'Asia/Ho_Chi_Minh'],
-  async () => {
-    console.log('Log for test deploy quirrel')
-  }
-)
+export const action = async () => {
+  console.log('Log for test qstash')
+}
